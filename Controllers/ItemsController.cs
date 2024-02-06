9ddb80f14296eacba5fcf63d815ddb04b7075e56
Rana <ranaberi25@gmail.com>
2024-02-06 13:14:00 +0100

    Get item request added

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -12,6 +12,9 @@ public class ItemsController : ControllerBase
 {
     private readonly InMemItemsRepository repository;
 
+/// <summary>
+/// Everytime a request is made to the controller, a new instance of the InMemItemsRepository() is created 
+/// </summary>
     public ItemsController()
     {
         repository = new InMemItemsRepository();
@@ -22,6 +25,18 @@ public class ItemsController : ControllerBase
     {
         var items = repository.GetItems();
         return items;
+    }
+
+    // Get/items/{id}
+    //ActionResult allows us to return a httpAction result or the object
+    [HttpGet("{id}")]
+    public ActionResult< Item> GetItem(Guid id)
+    {
+        var item = repository.GetItem(id);
+        if(item is null){
+            return NotFound();
+        }
+        return item;
     }
 }
 }
