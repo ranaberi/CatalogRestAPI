a0cef65389f11f3fb4e3f85b344f5eeb662b0519
Rana <ranaberi25@gmail.com>
2024-02-11 17:01:32 +0100

    fluent assertions added

--- CatalogUnitTests/ItemsControllerTests.cs
+++ CatalogUnitTests/ItemsControllerTests.cs
@@ -3,22 +3,24 @@ using Microsoft.Extensions.Logging;
 using CatalogApi.Controllers;
 using CatalogApi.Repositories;
 using CatalogApi.Entities;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Threading.Tasks;
-using Xunit;
+using FluentAssertions;
+
+
 namespace CatalogUnitTests;
 
 public class ItemsControllerTests
 {
+    private readonly Mock<IItemsRepository> repositoryStub = new();
+    private readonly Mock<ILogger<ItemsController>> loggerStub = new();
+
+    private readonly Random rand = new();
+
     [Fact]
     public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
     {
         //Arrange
-        var repositoryStub = new Mock<IItemsRepository>();
         repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync((Item)null);
 
-        var loggerStub = new Mock<ILogger<ItemsController>>();
         var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
 
         //Act
@@ -26,6 +28,35 @@ public class ItemsControllerTests
 
         //Assert
         Assert.IsType<NotFoundResult>(result.Result);
+        result.Result.Should().BeOfType<NotFoundResult>();
 
+    }
+
+    [Fact]
+    public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+    {
+        //Arrange
+        var expectedItem = CreateRandomItem();
+        repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync(expectedItem);
+
+        var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+        //Act
+        var result = await controller.GetItemAsync(Guid.NewGuid());
+
+        //Assert
+        result.Value.Should().BeEquivalentTo(, options => options.ComparingByMembers<Item>());
+    }
+
+    private Item CreateRandomItem()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = Guid.NewGuid().ToString(),
+            Price = rand.Next(5000),
+            CreatedDate = DateTimeOffset.UtcNow
+
+        };
     }
 }
